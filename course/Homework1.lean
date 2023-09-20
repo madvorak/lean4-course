2a1401367a13a26833fc003e042f014b8a7e12ef
madvorak <martin.dvorak@matfyz.cz>
2023-09-20 11:44:53 +0200

    sample outputs

--- course/Homework1.lean
+++ course/Homework1.lean
@@ -3,37 +3,37 @@ import course.Day1
 
 def is4thpow (a : Nat) : Bool := false -- TODO
 
-#eval is4thpow 15
-#eval is4thpow 16
-#eval is4thpow 17
-#eval is4thpow 0
-#eval is4thpow 1
-#eval is4thpow 2
-#eval is4thpow 3
-#eval is4thpow 4
-#eval is4thpow 5
+#eval is4thpow 15 -- false
+#eval is4thpow 16 -- true
+#eval is4thpow 17 -- false
+#eval is4thpow 0 -- true
+#eval is4thpow 1 -- true
+#eval is4thpow 2 -- false
+#eval is4thpow 3 -- false
+#eval is4thpow 4 -- false
+#eval is4thpow 5 -- false
 #eval List.filter is4thpow (List.range 5000)
 
 
 def isPerfectNumber (a : Nat) : Bool := false -- TODO
 
-#eval isPerfectNumber 5
-#eval isPerfectNumber 6
-#eval isPerfectNumber 7
-#eval isPerfectNumber 12
-#eval isPerfectNumber 28
-#eval isPerfectNumber 30
+#eval isPerfectNumber 5 -- false
+#eval isPerfectNumber 6 -- true
+#eval isPerfectNumber 7 -- false
+#eval isPerfectNumber 12 -- false
+#eval isPerfectNumber 28 -- true
+#eval isPerfectNumber 30 -- false
 
 
 def prefixSums : List Int → List Int := id -- TODO
 
-#eval prefixSums [1, 2, 5, 0]
+#eval prefixSums [1, 2, 5, 0] -- [1, 3, 8, 8]
 #eval prefixSums [1, -5, 3, 2, 2, 2, 2]
 #eval prefixSums [0, 0, 10, -1, -2, -3, -4, -5, 0, 10, 0]
 
 
 def postfixSums : List Int → List Int := id -- TODO
 
-#eval postfixSums [1, 2, 5, 0]
+#eval postfixSums [1, 2, 5, 0] -- [8, 7, 5, 0]
 #eval postfixSums [1, -5, 3, 2, 2, 2, 2]
 #eval postfixSums [0, 0, 10, -1, -2, -3, -4, -5, 0, 10, 0]
